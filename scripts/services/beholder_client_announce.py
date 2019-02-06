d9818b0cb15fe711ba9b34c9ccc2ce63baa79a39
Ronny Eichler <reichler@science.ru.nl>
2019-02-06 21:50:54 +0100

    Switch announcing service to UDP broadcast

--- scripts/services/beholder_client_announce.py
+++ scripts/services/beholder_client_announce.py
@@ -4,33 +4,62 @@ import socket
 import time
 
 PORT = 18000
-HOST = '131.174.140.26'
+HOST = '131.174.140.255'
 
 RECONNECT_WAIT = 3
 SEND_INTERVAL = 3
 
-def my_ip():
-    return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0] + '\n'
+TYPE = 'UDP'
 
-def hostname():
+
+def my_ip():
+    return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
+        [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+         [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0] + '\n'
+
+
+def my_hostname():
     return socket.gethostname()
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        print('Connecting...')
-        try:
-            s.connect((HOST, PORT))
-        except (ConnectionRefusedError, OSError) as e:
-            print('Connection failed!', e)
-        else:
-            while True:
-                info = '{}: {}'.format(hostname(), my_ip())
-                print('Sending', info)
-                try:
-                    s.sendall(str.encode(info))
-                    time.sleep(SEND_INTERVAL)
-                except BrokenPipeError:
-                    s.close()
-                    break
-    time.sleep(RECONNECT_WAIT)
-                    
+
+def serve_tcp():
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print('Connecting...')
+            try:
+                s.connect((HOST, PORT))
+            except (ConnectionRefusedError, OSError) as e:
+                print('Connection failed!', e)
+            else:
+                while True:
+                    info = '{}:{}'.format(my_hostname(), my_ip())
+                    print('Sending', info)
+                    try:
+                        s.sendall(str.encode(info))
+                        time.sleep(SEND_INTERVAL)
+                    except (BrokenPipeError, ConnectionResetError) as e:
+                        print('Connection closed:', e)
+                        s.close()
+                        break
+        time.sleep(RECONNECT_WAIT)
+
+
+def serve_udp():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        while True:
+            info = '{}:{}'.format(my_hostname(), my_ip())
+            print('Sending', info)
+            s.sendto(str.encode(info), ('<broadcast>', 50101))
+            time.sleep(SEND_INTERVAL)
+
+
+if __name__ == '__main__':
+    if TYPE == 'TCP':
+        serve_tcp()
+
+    elif TYPE == 'UDP':
+        serve_udp()
+
+    else:
+        raise NotImplementedError
