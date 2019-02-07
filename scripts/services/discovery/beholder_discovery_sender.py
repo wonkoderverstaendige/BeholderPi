f336bd9fd1c60ab42ce0ba3c809ec5ce5bc2fbef
Ronny Eichler <reichler@science.ru.nl>
2019-02-08 00:43:53 +0100

    Fix issue with hostname resolution in discovery sender

--- scripts/services/discovery/beholder_discovery_sender.py
+++ scripts/services/discovery/beholder_discovery_sender.py
@@ -20,8 +20,10 @@ TYPE = 'UDP'
 
 
 def my_ip():
-    return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
-        [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+    # return (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
+    #     [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+    #      [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0] + '\n'
+    return (([[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
          [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0] + '\n'
 
 
