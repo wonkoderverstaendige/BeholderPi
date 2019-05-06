4eb164c2c18351515dd89cf7884fdccdc603c003
Ronny Eichler <reichler@science.ru.nl>
2019-05-06 02:16:01 +0200

    Messing with ffmpeg options testing nvenc

--- beholder/defaults.py
+++ beholder/defaults.py
@@ -8,13 +8,20 @@ PIEYE_SUB_TOPIC = b'PYSPY'
 FFMPEG_BINARY = 'ffmpeg'
 
 FFMPEG_COMMAND = [FFMPEG_BINARY,
-                  '-y', '-hide_banner',
+                  '-y',
+                  '-hide_banner',
+                  '-loglevel', 'error',
+                  '-nostats',
                   '-f', 'mjpeg',
+                  # '-hwaccel', 'nvdec',
                   '-r', '30.',
                   '-i', '-',
+                  # '-pix_fmt', 'yuv420p',
                   '-c:v', 'libx264',
+                  # '-c:v', 'h264_nvenc',
                   # '-c:v', 'copy',
-                  '-preset', 'veryfast',
+                  '-preset', 'fast',
+                  # '-profile:v', 'high',
                   '-vf', 'hqdn3d'
                   ]
 
