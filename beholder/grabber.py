03487deb93611b280e458be8c6042bdad8f4d19b
Ronny Eichler <r.eichler@science.ru.nl>
2019-05-12 02:48:48 +0200

    Factor out frame annotation

--- beholder/grabber.py
+++ beholder/grabber.py
@@ -34,6 +34,7 @@ class Grabber(threading.Thread):
         self.poller = zmq.Poller()
 
         self.timed_out = False
+        self.faulted = False
 
         self.n_frames = 0
         self.frame = None
@@ -48,9 +49,6 @@ class Grabber(threading.Thread):
 
         self.transpose = transpose
 
-        # shape = (self.height + FRAME_METADATA_H, self.width, self.colors)
-        # num_bytes = int(np.prod(shape))
-
         self.fault_frame = cv2.resize(FAULTY_FRAME, (self.width, self.height))
         self.no_signal_frame = cv2.resize(NO_SIGNAL_FRAME, (self.width, self.height))
 
@@ -58,7 +56,6 @@ class Grabber(threading.Thread):
         with arr.get_lock():
             self._shared_arr = arr
             logging.debug('{} shared array: {}'.format(self.name, arr))
-            # self._fresh_frame = buf_to_numpy(arr, shape=shape, offset=self.id * num_bytes, count=num_bytes)
             self._fresh_frame = buf_to_numpy(arr, shape=(
                 self.height * self.cfg['n_rows'], self.width * self.cfg['n_cols'], 3))
             logging.debug('Numpy shared buffer at {}'.format(hex(self._fresh_frame.ctypes.data)))
@@ -83,6 +80,8 @@ class Grabber(threading.Thread):
         logging.debug('Starting loop in {}!'.format(self.name))
         t0 = cv2.getTickCount()
 
+        annotate = True
+
         # Loop while stop flag not set
         while not self._ev_terminate.is_set():
             # Poll socket for incoming messages, timeout when nothing comes in
@@ -100,6 +99,7 @@ class Grabber(threading.Thread):
 
             else:
                 # Source reconnected
+                self.faulted = False
                 if self.timed_out:
                     self.timed_out = False
                     logging.info('Frame source connected')
@@ -141,11 +141,11 @@ class Grabber(threading.Thread):
 
             # If no valid frame was decoded even though we did receive something, show a warning
             if self.frame is None:
+                self.faulted = True
                 self.frame = self.fault_frame
 
-            # # Make space for the metadata bar at the bottom of each frame
-            # frame.resize((frame.shape[0] + FRAME_METADATA_H, frame.shape[1], frame.shape[2]))
-            # self.frame = Frame(self.n_frames, frame, 'Grabber', add_stamps=self.is_live)
+            if not self.timed_out and not self.faulted:
+                self.annotate_frame()
 
             # Send frames to attached threads/processes
             self.relay_frames()
@@ -155,63 +155,30 @@ class Grabber(threading.Thread):
             self._t_loop.appendleft((cv2.getTickCount() - t0) / cv2.getTickFrequency() * 1000)
             t0 = cv2.getTickCount()
 
-            # Every now and then show fps
-            # if not self.n_frames % N_FRAMES_FPS_LOG:
-            #     avg_fps = 1 / (sum(self._t_loop) / len(self._t_loop))
-            #     logging.debug(
-            #         'Grabbing frame {}... {}, avg. {:.1f} fps'.format(self.n_frames, 'OK' if rt else 'FAIL', avg_fps))
-
         logging.debug('Stopping loop in {}!'.format(self.name))
         self.close()
 
-    # def embed_metadata(self, row, label, data):
-    #     """Embed metadata into pixels. Note: Data has to be int64-able.
-    #     """
-    #     line = np.zeros(FRAME_METADATA_BYTE, dtype=np.uint8)
-    #     line[0] = np.array([self.id], dtype=np.uint8)
-    #     line[1:7] = np.fromstring('{:<6s}'.format(label), dtype=np.uint8)
-    #     line[7:] = np.array([data], dtype=np.uint64).view(np.uint8)
-    #     self._fresh_frame[-FRAME_METADATA_H + row:-FRAME_METADATA_H + row + 1, \
-    # -FRAME_METADATA_BYTE // 3:] = line.reshape(1, -1, 3)
+    def annotate_frame(self):
+        # vertical lines
+        ch_color = (0, 0, 0)
+        th_thickness = 1
+        cv2.line(self.frame, (300, 350), (300, 390), ch_color, thickness=th_thickness)
+        cv2.line(self.frame, (300, 410), (300, 450), ch_color, thickness=th_thickness)
+
+        # horizontal lines
+        cv2.line(self.frame, (250, 400), (290, 400), ch_color, thickness=th_thickness)
+        cv2.line(self.frame, (310, 400), (350, 400), ch_color, thickness=th_thickness)
 
     def relay_frames(self):
         """Forward acquired image to entities downstream via queues or shared array.
         """
-
-        # # FPS display
-        # if len(self._t_loop):
-        #     fps_str = 'G={:.1f}fps'.format(1000 / (sum(self._t_loop) / len(self._t_loop)))
-        # else:
-        #     fps_str = 'G=??.?fps'
-        # cv2.putText(self.frame.img, fps_str, (270, self.frame.img.shape[0] - 5), FONT, 1.0,
-        #             (255, 255, 255), lineType=cv2.LINE_AA)
-
         # Forward frame for tracking and display
-        # NOTE: [:] indicates to reuse the buffer
-
-        # vertical lines
-        cv2.line(self.frame, (300, 350), (300, 390), (255, 255, 255), thickness=3)
-        cv2.line(self.frame, (300, 410), (300, 450), (255, 255, 255), thickness=3)
-
-        # horizontal lines
-        cv2.line(self.frame, (250, 400), (290, 400), (255, 255, 255), thickness=3)
-        cv2.line(self.frame, (310, 400), (350, 400), (255, 255, 255), thickness=3)
-
-        # cv2.line(self.frame, (dx, dy), (dx + 600, dy + 800), (255, 255, 255), thickness=1)
-        # cv2.line(self.frame, (dx, dy + 800), (dx + 600, dy), (255, 255, 255), thickness=1)
-        # cv2.line(self.frame, (dx + 300, dy + 410), (dx + 300, dy + 450), (255, 255, 255), thickness=1)
-
         try:
             # with self._shared_arr.get_lock():
-            self._fresh_frame[
-            self.height * self.n_row:self.height * (self.n_row + 1),
-            self.width * self.n_col:self.width * (self.n_col + 1), :] = self.frame
+            self._fresh_frame[self.height * self.n_row:self.height * (self.n_row + 1),
+                              self.width * self.n_col:self.width * (self.n_col + 1), :] = self.frame
         except ValueError as e:
             logging.debug(('VE', self.n_col, self.n_row, self._fresh_frame.shape, e))
-            # logging.info('test')
-            # self.embed_metadata(row=0, label='index', data=self.frame.index)
-            # self.embed_metadata(row=1, label='tickst', data=self.frame.tickstamp)
-            # self.embed_metadata(row=2, label='timest', data=int(self.frame.timestamp))
 
     def close(self):
         pass
