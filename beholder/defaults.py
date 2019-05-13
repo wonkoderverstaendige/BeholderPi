85fb10381c9cfd1994206e148a2faf1b11861c59
Ronny Eichler <r.eichler@science.ru.nl>
2019-05-13 03:51:15 +0200

    Hardware encoding!!!

--- beholder/defaults.py
+++ beholder/defaults.py
@@ -7,20 +7,31 @@ PIEYE_SUB_TOPIC = b'piEye'
 
 FFMPEG_BINARY = 'ffmpeg'
 
+
+# Note that as of Ubuntu 18.04 using ffmpeg installed with snap has
+# intermittent failures acquiring a CUDA context. This can be worked around
+# by installing the ffmpeg snap in --devmode
 FFMPEG_COMMAND = [FFMPEG_BINARY,
                   '-y',
                   '-hide_banner',
-                  '-loglevel', 'error',
+                  # '-loglevel', 'error',
                   '-nostats',
+
+                  # '-hwaccel', 'cuvid',
+                  # '-c:v', 'mjpeg_cuvid',
+
+                  # '-pix_fmt', 'yuvj420p',
                   '-f', 'mjpeg',
-                  # '-hwaccel', 'nvdec',
                   '-r', '30.',
                   '-i', '-',
-                  # '-pix_fmt', 'yuv420p',
-                  '-c:v', 'libx264',
-                  # '-c:v', 'h264_nvenc',
+# '-filter:v hwupload_cuda,scale_npp=format=nv12:interp_algo=lanczos,hwdownload,format=nv12',
+                  # '-c:v', 'libx264',
+                  '-c:v', 'h264_nvenc',
+                  '-b:v', '250k',
+                  # '-gpu', 'list',
                   # '-c:v', 'copy',
-                  '-preset', 'fast',
+                  '-preset', 'slow',
+                  '-pix_fmt', 'yuv420p',
                   # '-profile:v', 'high',
                   '-vf', 'hqdn3d'
                   ]
--- docs/Beholder_Installation.md
+++ docs/Beholder_Installation.md
@@ -1,14 +1,15 @@
 # Hardware
 
 
-
 # Software
 
 - default Ubuntu 18.04
 - install drivers for r8125 NIC
 
 - install new nvidia drivers (418)
-- install snap ffmpeg
+
+##### THIS IS IMPORTANT FOR HARDWARE ACCELERATION TO WORK!
+- install snap ffmpeg -- devmode
 
 # NTP/Chrony
 - install chrony
