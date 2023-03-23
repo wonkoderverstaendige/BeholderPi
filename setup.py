af2113cf8155d3cd78efe569276e0e5c81538f8d
Ronny Eichler <wonkoderverstaendige@gmail.com>
2023-03-23 15:07:40 +0100

    Streamlined install requirements

--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# Most packages are available from conda and we can get the tricky
+# opencv package via `conda install -c conda-forge opencv` built with qt
+conda_packs = []
+
+# But if not using conda, we can go with the pip available package:
+pip_packs = {'pip': ['opencv-contrib-python', 'numpy', 'networkx', 'pyzmq', 'pyyaml']}
+
 setup(name='beholderpi',
       description='BeholderPi',
       author='Ronny Eichler',
       author_email='ronny.eichler@gmail.com',
       version='0.1.5',
       packages=['beholder', 'piEye'],
-      install_requires=['numpy', 'opencv-contrib-python', 'networkx', 'pyzmq', 'pyyaml'],
+      install_requires=conda_packs,
+      extras_require=pip_packs,
       # entry_points="""[console_scripts]
       #       beholder=beholder.main:main"""
       )
